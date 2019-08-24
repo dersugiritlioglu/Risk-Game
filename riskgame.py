319928f7c58a4034b9cc65a851dc84b999eb479c
Dersu Giritlioglu <dgiritlioglu@gmail.com>
2019-08-25 02:47:29 +0300

    Update riskgame.py
    players_pick_fields function fixed.
    Minor changes in representations.

--- riskgame.py
+++ riskgame.py
@@ -28,26 +28,31 @@ def print_field(field):
         print(i, "  ", end='')
         for j in range(field.shape[1]):
             if field[i][j] == -1:
-                print(".  ", end = '')
+                print("x  ", end = '')
             else:
-                print(str(int(field[i][j]))+"  ", end = '')
+                if field[i][j] == -2:
+                    print(".  ", end = '')
+                else:
+                    print(str(int(field[i][j]))+"  ", end = '')
         print()
         
 def formfield(field):
-    empty = 0
+    empty = []
     for i in range(field.shape[0]):
         for j in range(field.shape[1]):
             if np.random.random() < sparsity:
                 field[i][j] = -1
-                empty += 1
+            else:    
+                empty.append((i,j))
     return empty
 
 def players_pick_fields(field, pl_count, empty):
-    print('Now you will pick spots from the field. Highest dice will pick first.')
+    print('Now you will pick spots from the field. Dots are the places you can select,\
+         x\'s represent the sea, so noone can take them. Highest dice will pick first.\n')
     dicememo = np.zeros(pl_count)
-    starting_pl_count = pl_count
+    starting_pl_count = list(range(pl_count))
     while True:
-        for i in range(starting_pl_count):
+        for i in starting_pl_count:
             print('Player '+ str(i) + ', roll the dice (press Enter).')
             with Listener(
                     on_press=on_press,
@@ -60,35 +65,32 @@ def players_pick_fields(field, pl_count, empty):
             starting_player=np.where(dicememo==np.max(dicememo))[0]
             break
         else:
-            starting_pl_count = np.where(dicememo==np.max(dicememo))[0].shape[0]
-            # TODO: player ismi kayıyor. Bir liste yapıp whilein içindeki foru o listede döndürebilirim.
+            #starting_pl_count = np.where(dicememo==np.max(dicememo))[0].shape[0]
+            starting_pl_count = list(np.where(dicememo==np.max(dicememo))[0])
+            # TODO: PROBABLY DONE... player ismi kayıyor. Bir liste yapıp whilein içindeki foru o listede döndürebilirim.
     print('Player '+ str(starting_player[0]) + ' picks first.')
-        
-    while empty>0:
+    with Listener(
+            on_press=on_press,
+            on_release=on_release_enter) as listener:
+        listener.join()
+
+    while len(empty)>0:
+        # start from the starting player
+        # the other dices mean nothing. Order will be clockwise (in this case,
+        # increasing order).
         for pid in range(starting_player[0], starting_player[0]+pl_count):
             pid = pid%pl_count
-            picked = np.random.randint(0,width*height - empty)
-            filledcount = 0
-            for h in range(height):
-                breakflag = False
-                for w in range(width):    
-                    if field[h][w] == 0:
-                        if filledcount == picked:
-                            print("f,p:",filledcount,picked)
-                            field[h][w] = pid
-                            print("Player ", pid, ": ", h, ",", w)
-                            breakflag = True
-                            break
-                        filledcount += 1
-                if breakflag:
-                    break
-            with Listener(
-                    on_press=on_press,
-                    on_release=on_release_enter) as listener:
-                listener.join()
-        print_field(field)
-        empty -= 1
-        # TODO: fix this function
+            picked = np.random.randint(len(empty))
+            h,w = empty[picked]
+            empty.remove((h,w))
+            field[h][w] = pid
+            print("Player ", pid, ": ", h, ",", w)
+            print_field(field)
+            if len(empty)>0:
+                with Listener(
+                        on_press=on_press,
+                        on_release=on_release_enter) as listener:
+                    listener.join()
 
     print('Starting spots are set!')
         
@@ -99,11 +101,11 @@ global height
 
 width = 10
 height = 10
-sparsity = 0.1
+sparsity = 0.4
 #pl_count = int(input("How many players? : "))
 pl_count = 3
 
-whos_field = np.full((width,height),9)
+whos_field = np.full((width,height),-2) # -2 indicated not selected yet in the field representation.
 empty = formfield(whos_field)
 print_field(whos_field)
 players_pick_fields(whos_field, pl_count, empty)
