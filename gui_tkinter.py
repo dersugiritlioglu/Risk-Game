91841ce9cca0d5d0d52f7e86670067c58551d080
dersugiritlioglu <dgiritlioglu@gmail.com>
2020-03-16 16:52:59 +0300

    3A-3D added
    In capital conquest mode, capitals defend with 3 dice. Added the probabilities for this battle.

---
+++ gui_tkinter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 21:29:48 2019
+
+@author: lenovo
+"""
+
+import tkinter as tk
+
+win = tk.Tk()
+
+win.title('dersu')
+
+win.mainloop()
--- risk_probabilities.py
+++ risk_probabilities.py
@@ -82,7 +82,59 @@ for a1 in dice:
 print("3A - 2D: ", "{:12.2f}".format(a_2dead/6**5*100), "{:21.2f}".format(draw/6**5*100), "{:17.2f}".format(d_2dead/6**5*100))
 ###############################################################################
 
+
+
+# 3A - 3D
+a_2dead = 0
+draw = 0
+d_2dead = 0
+
+
+for a1 in dice:
+    for a2 in dice:
+        for a3 in dice:
+            for d1 in dice:
+                for d2 in dice:
+                    for d3 in dice:
+                        a_death = 0
+                        d_death = 0
+                        a_max = max(a1,a2,a3)
+                        d_max = max(d1,d2,d3)
+                        if d_max >= a_max:
+                            a_death += 1
+                        else:
+                            d_death += 1
+                        a_others = [a1,a2,a3]
+                        a_others.remove(a_max)
+                        d_others = [d1,d2,d3]
+                        d_others.remove(d_max)
                         
+                        a_max2 = max(a_others)
+                        d_max2 = max(d_others)
+                        if d_max2 >= a_max2:
+                            a_death += 1
+                        else:
+                            d_death += 1
+                        
+                        a_others.remove(a_max2)
+                        d_others.remove(d_max2)
+                        
+                        if a_others[0] >= d_others[0]:
+                            a_death += 1
+                        else:
+                            d_death += 1
+                        
+                        if a_death > d_death:
+                            a_2dead += 1
+                        elif a_death == d_death:
+                            draw += 1
+                        else:
+                            d_2dead += 1
+
+                        
+print("3A - 3D: ", "{:12.2f}".format(a_2dead/6**6*100), "{:21.2f}".format(draw/6**6*100), "{:17.2f}".format(d_2dead/6**6*100))
+
+###############################################################################                       
 # 2A - 2D
 a_2dead = 0
 draw = 0
